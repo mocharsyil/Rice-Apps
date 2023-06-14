5acf431ab61aeab763e4ee7a8a684e54505fa377
mocharsyil <moch.arsyil@gmail.com>
2023-06-14 22:55:35 +0700

    Revert "Revert "add api""
    This reverts commit 989fdfbc202a8ad33d2711417cc1a6767ce39a4f.

--- app/src/main/java/com/bangkit/haze/LoginActivity.kt
+++ app/src/main/java/com/bangkit/haze/LoginActivity.kt
@@ -2,19 +2,25 @@ package com.bangkit.haze
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.bangkit.haze.api.ApiClient
+import com.bangkit.haze.api.AuthService
+import com.bangkit.haze.model.AuthenticationRequest
+import com.bangkit.haze.model.AuthenticationResponse
 
-
-
-
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var editTextName: EditText
     private lateinit var editTextPassword: EditText
     private lateinit var buttonLogin: Button
+    private lateinit var authService: AuthService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,22 +39,56 @@ class LoginActivity : AppCompatActivity() {
         editTextPassword = findViewById(R.id.loginPassword)
         buttonLogin = findViewById(R.id.login)
 
+        authService = ApiClient.createService(AuthService::class.java)
+
+
         buttonLogin.setOnClickListener {
             val name = editTextName.text.toString()
             val password = editTextPassword.text.toString()
 
-            // Perform login validation here
-            // Example code:
-            if (name == "admin" && password == "password") {
-                // Login successful, navigate to the next activity
-                // Replace MainActivity::class.java with your desired activity
-                val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
-                startActivity(intent)
-                finish()
-            } else {
-                // Show error message or handle invalid login credentials
-                Toast.makeText(this@LoginActivity, "Invalid credentials", Toast.LENGTH_SHORT).show()
-            }
+            // Create authentication request
+            val request = AuthenticationRequest(name, password)
+
+            // Call the authentication API
+            val call = authService.authenticate(request)
+            call.enqueue(object : Callback<AuthenticationResponse> {
+                override fun onResponse(
+                    call: Call<AuthenticationResponse>,
+                    response: Response<AuthenticationResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        // Login successful, get the access token
+                        val accessToken = response.body()?.data?.accessToken
+
+                        // Save the access token to a shared preference or wherever you store user session data
+                        // ...
+
+                        // Navigate to the next activity
+                        // Replace DashboardActivity::class.java with your desired activity
+                        val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // Show error message or handle invalid login credentials
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Invalid credentials",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    }
+                }
+
+                override fun onFailure(call: Call<AuthenticationResponse>, t: Throwable) {
+                    // Show error message or handle API call failure
+                    val errorMessage = "Failed to authenticate"
+                    Toast.makeText(this@LoginActivity, errorMessage, Toast.LENGTH_SHORT).show()
+
+                    // Log the failure message
+                    Log.d("LoginActivity", errorMessage)
+                }
+
+            })
         }
 
         val notHaveAnAccountTextView = findViewById<TextView>(R.id.not_have_an)
---
+++ app/src/main/java/com/bangkit/haze/model/AuthenticationRequest.kt
@@ -0,0 +1,6 @@
+package com.bangkit.haze.model
+
+data class AuthenticationRequest(
+    val username: String,
+    val password: String
+)
